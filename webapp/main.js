270bc746d50e01c7ea64e96c7cf3728be75ad000
Naoyuki Sato <naoyuki.sato@gmail.com>
2013-01-13 23:11:31 +0900

    clean up source code and add comments

--- webapp/main.js
+++ webapp/main.js
@@ -1,6 +1,10 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('ssdp.html', {
-    width: 680,
-    height: 480
-  });
+chrome.app.runtime.onLaunched.addListener(function()
+{
+	/*
+	 * Create Web app windows w 680 x 480
+	 */
+	chrome.app.window.create('ssdp.html', {
+		width: 680,
+		height: 480
+	});
 });
--- webapp/ssdp.js
+++ webapp/ssdp.js
@@ -3,89 +3,115 @@ console.debug = function() {};
 var output = null;
 var ssdpData = "";
 
-window.addEventListener("load", function() {
-  console.log("SSDP multicast app starts");
-  var connect = document.getElementById("start");
-  connect.onclick = ssdpStart;
+/*
+ * This function will be called when this js is loaded.
+ */
+window.addEventListener("load", function()
+{
+    console.log("SSDP multicast app starts");
+    var connect = document.getElementById("start");
+    connect.onclick = ssdpStart;
 
-  output = document.getElementById("output");
+    output = document.getElementById("output");
 });
 
-// translate text string to Arrayed buffer
-function t2ab(str /* String */) {
+
+/*
+ * translate text string to arryed buffer
+ */
+function t2ab(str /* String */)
+{
     var buffer = new ArrayBuffer(str.length);
     var view = new DataView(buffer);
-    for(var i = 0, l = str.length; i < l; i++) {
-      view.setInt8(i, str.charAt(i).charCodeAt());
+    for(var i = 0, l = str.length; i < l; i++)
+    {
+        view.setInt8(i, str.charAt(i).charCodeAt());
     }
     return buffer;
 }
 
-  // translate Arrayed buffer to text string
-  //
-function ab2t(buffer /* ArrayBuffer */) {
+/*
+ * translate arrayed buffer to text string
+ */
+function ab2t(buffer /* ArrayBuffer */)
+{
     var arr = new Int8Array(buffer);
     var str = "";
-    for(var i = 0, l = arr.length; i < l; i++) {
-      str += String.fromCharCode.call(this, arr[i]);
+    for(var i = 0, l = arr.length; i < l; i++)
+    {
+        str += String.fromCharCode.call(this, arr[i]);
     }
     return str;
 }
 
-var recieveData = function(socket, sid) {
-  socket.recvFrom(sid, function(recv) {
-    var data = ab2t(recv.data);
+/*
+ * This function will be called when upd packet is recieved
+ */
+var recieveData = function(socket, sid)
+{
+    socket.recvFrom(sid, function(recv)
+    {
+        var data = ab2t(recv.data);
 
-    var dt = new Date();
-    var tmp = dt + "<br>";
-    tmp += data.replace(/"\r\n"/g, "<br>") + "<br><br>";
+        var dt = new Date();
+        var tmp = dt + "<br>";
+        tmp += data.replace(/"\r\n"/g, "<br>") + "<br><br>";
 
-    tmp += ssdpData;
+        tmp += ssdpData;
 
-    console.log(tmp);
-    output.innerHTML = tmp;
-    ssdpData = tmp;
+        console.log(tmp);
+        output.innerHTML = tmp;
+        ssdpData = tmp;
 
-    recieveData(socket, sid);
-  });
+        recieveData(socket, sid);
+    });
 };
 
 
-
-var ssdpStart = function() {
+/*
+ * This function will be called when "SSDP Start" button is pushed.
+ */
+var ssdpStart = function()
+{
+    // M-Search packed w/ "ssdp:all"
     var MSearchAll = "M-SEARCH * HTTP/1.1\r\n" +
         "ST: ssdp:all\r\n" +
         "MAN: \"ssdp:discover\"\r\n" +
         "HOST: 239.255.255.250:1900\r\n" +
         "MX: 10\r\n\r\n";
+    // chrome socket
     var socket = chrome.socket || chrome.experimental.socket;
-
+    // SSDP multicast address
     var SSDPMulticastAddress = "239.255.255.250";
+    // SSDP multicast port
     var SSDPMulticastPort = 1900;
+    // socket id
     var sid;
 
-    socket.create('udp', {}, function(socketInfo) {
+    // create udp socket
+    socket.create('udp', {}, function(socketInfo)
+    {
         sid = socketInfo.socketId;
-        console.log("sid: " + sid);
-        socket.bind(sid, "0.0.0.0", 0, function(res) {
+        console.log("socket id: " + sid);
+        socket.bind(sid, "0.0.0.0", 0, function(res)
+        {
             if(res !== 0) {
                 throw('cannot bind socket');
                 return -1;
             }
 
+            // recieve data
             recieveData(socket, sid);
 
-            // Send SSDP Search
+            // Send SSDP Search x 2
             var buffer = t2ab(MSearchAll);
-            var closure_ = function(e) {
-                if(e.bytesWritten < 0) {
-                    throw("an Error occured while sending M-SEARCH : "+e.bytesWritten);
-                }
-            }
-
-            for(var i = 0; i < 2; i++) {
-                socket.sendTo(sid, buffer, SSDPMulticastAddress, SSDPMulticastPort, function(e) {
-                closure_(e);
+            for(var i = 0; i < 2; i++)
+            {
+                socket.sendTo(sid, buffer, SSDPMulticastAddress, SSDPMulticastPort, function(e)
+                {
+                    if(e.bytesWritten < 0) {
+                        throw("an Error occured while sending M-SEARCH : "+e.bytesWritten);
+                    }
                 });
             }
         });
